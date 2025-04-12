f5b2170a01332314373647cfe394afdd2998e877
unknown <magalhaesv335@gmail.com>
2025-04-11 21:33:09 -0300

    Questão 7 não lembro o comando para rodar

---
+++ SistemaAcademico.WebAPI/Controllers/AlunoController.cs
@@ -0,0 +1,24 @@
+﻿namespace SistemaAcademico.WebAPI;
+
+public class AlunoController : ControllerBase
+    {
+        private readonly IAlunoServico _servico;
+
+        public AlunoController(IAlunoServico servico)
+        {
+            _servico = servico;
+        }
+
+        [HttpPost]
+        public IActionResult Post([Frombody]AlunoDto dto)
+        {
+            _servico.Cadastrar(dto);
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_servico.ListarTodos());
+        }
+    }
